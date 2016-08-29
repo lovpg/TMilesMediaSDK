b631ac22a6da8fb29132af0962edb3f0a940333c
gaozl <gaozl@olla.im>
2016-08-29 11:04:49 +0800

    多媒体处理初始化

---
+++ audio/LLAudioUpload.m
@@ -0,0 +1,19 @@
+//
+//  LLAudioUpload.m
+//  TMilesMediaSDK
+//
+//  Created by Reco on 16/8/29.
+//  Copyright © 2016年 广州市拾里信息科技有限公司. All rights reserved.
+//
+
+#import "LLAudioUpload.h"
+
+@implementation LLAudioUpload
+
+- (void) upload : (NSString *)filename
+           data : (NSData *)data
+{
+    NSLog(@"%@", filename);
+}
+
+@end
---
+++ vedio/LLVedioUpload.m
@@ -0,0 +1,20 @@
+//
+//  LLVedioUpload.m
+//  TMilesMediaSDK
+//
+//  Created by Reco on 16/8/29.
+//  Copyright © 2016年 广州市拾里信息科技有限公司. All rights reserved.
+//
+
+#import "LLVedioUpload.h"
+
+@implementation LLVedioUpload
+
+
+- (void) upload : (NSString *)filename
+           data : (NSData *)data
+{
+    NSLog(@"vedio upload: %@ ", filename);
+}
+
+@end
